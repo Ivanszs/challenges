297a0febd7ad84a8545cd8f790ea87ad56cf58cf
Ivan Sz <68451124+Ivanszs@users.noreply.github.com>
2020-11-13 11:49:52 -0300

    Add files via upload

---
+++ maxPairwiseProduct.js
@@ -0,0 +1,17 @@
+function max(arr) {
+    let maxNumber = 0;
+    let secondMaxNumber = 0;
+    for (let a = 0; a < arr.length; a++) {
+        if (arr[a] > maxNumber) {
+            maxNumber = arr[a];
+        }
+    }
+    for (let b = 0; b < arr.length; b++) {
+        if (arr[b] == maxNumber) {
+            continue;
+        } else if (arr[b] > secondMaxNumber) {
+            secondMaxNumber = arr[b];
+        }
+    }
+    return maxNumber * secondMaxNumber;
+}
