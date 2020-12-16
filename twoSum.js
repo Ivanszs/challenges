a0c81bb49366c0546897ee45f44df50baf43a6fc
Ivan Sz <68451124+Ivanszs@users.noreply.github.com>
2020-12-16 11:35:57 -0300

    Update and rename TwoSum.js to twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,6 @@
-/*returns indexes of 2 numbers that summed are equal to the target parameter*/
+/*
+returns indexes of 2 numbers that summed are equal to the target parameter
+*/
 let twoSum = function(nums, target) {
   output = [];
   for (let i = 0; i < nums.length; i++) {
