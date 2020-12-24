a089372efa40033b9b235d3bf6044051792a75a9
Ivan Sz <68451124+Ivanszs@users.noreply.github.com>
2020-12-24 19:45:22 -0300

    Create sortArrByParity.js

---
+++ sortArrByParity.js
@@ -0,0 +1,22 @@
+/*
+Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+You may return any answer array that satisfies this condition.
+example:
+Input: [3,1,2,4]
+Output: [2,4,3,1]
+The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+*/
+const sortArrayByParity = function(A) {
+  let narr = [];
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] % 2 == 0 ) {
+      narr.push(A[i]);
+    }
+  }
+  for(let j = 0; j < A.length; j++) {
+    if(A[j] % 2 != 0) {
+      narr.push(A[j]);
+    }
+  }
+  return narr
+};
