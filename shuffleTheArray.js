9e4b0b98df6b2d9ec9135b2c7e34afaaec03bc5a
Ivan Sz <68451124+Ivanszs@users.noreply.github.com>
2021-01-03 22:49:39 -0300

    Create shuffleTheArray.js

---
+++ shuffleTheArray.js
@@ -0,0 +1,16 @@
+/*
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+Example:
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+*/
+const shuffle = function(nums, n) {
+  let arr = [];
+  for (let i = n; i < nums.length; i++) {
+    arr.push(nums[i-n]);
+    arr.push(nums[i]);
+  }
+  return arr;
+};
